0c81b9ee7fecc5931f0f5857353a4301d314fa0b
Fusedev <47820634+fusedevgithub@users.noreply.github.com>
2022-03-23 21:21:34 +0700

    Termux support lamo

---
+++ termux.sh
@@ -0,0 +1,21 @@
+echo "
+╱╱╱╱╱╱╱╱╱╱╱╭╮╱╭╮
+╱╱╱╱╱╱╱╱╱╱╱┃┃╱┃┃
+╭━━┳╮╭┳━━┳━╯┣━╯┣━━┳━━╮
+┃━━┫┃┃┃━━┫╭╮┃╭╮┃╭╮┃━━┫
+┣━━┃╰╯┣━━┃╰╯┃╰╯┃╰╯┣━━┃
+╰━━┻━━┻━━┻━━┻━━┻━━┻━━╯"
+echo We are not responsible for any damage caused by this script.
+sleep 1
+pkg update -y 
+pkg install python3-pip -y  
+pkg install python3 -y  
+​mkdir ATTACK-PACK ​&&​ ​cd​ ATTACK-PACK  
+​git clone https://github.com/Leeon123/TCP-UDP-Flood  
+​git clone https://github.com/MHProDev/MHDDoS  
+​sudo apt-get install wget -y  
+​sudo apt-get install unzip -y ​&&​ mkdir MHDDoS-old-but-powerful  
+​cd​ MHDDoS-old-but-powerful ​&&​ wget https://github.com/MHProDev/MHDDoS/releases/download/Beta/MHDDoS.v1.-.Beta.zip  
+​unzip MHDDoS.v1.-.Beta.zip ​&&​ ​cd​ .. ​&&​ ​cd​ MHDDoS  
+pip3 install -r requirements.txt ​&&​ ​cd​ ​~ 
+echo​ ​"​susddos is installing done! now cd ATTACK-PACK!​"
