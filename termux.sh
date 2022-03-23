ca55f49923f4a393518a7877acf356ed8256d556
Fusedev <47820634+fusedevgithub@users.noreply.github.com>
2022-03-23 21:35:09 +0700

    TRYING FIX COMMAND NOT FOUND

--- termux.sh
+++ termux.sh
@@ -5,16 +5,12 @@ echo "
 ┃━━┫┃┃┃━━┫╭╮┃╭╮┃╭╮┃━━┫
 ┣━━┃╰╯┣━━┃╰╯┃╰╯┃╰╯┣━━┃
 ╰━━┻━━┻━━┻━━┻━━┻━━┻━━╯"
-echo We are not responsible for any damage caused by this script.
 sleep 1
-pkg update -y
-pkg install python3-pip -y
-pkg install python3 -y
+echo We are not responsible for any damage caused by this script.
 ​mkdir ATTACK-PACK ​&&​ ​cd​ ATTACK-PACK
 ​git clone https://github.com/Leeon123/TCP-UDP-Flood
 ​git clone https://github.com/MHProDev/MHDDoS
-​pkg install wget -y
-​pkg install unzip -y ​&&​ mkdir MHDDoS-old-but-powerful
+pkg update -y && pkg install python3-pip -y && pkg install python3 -y && ​pkg install wget -y && pkg install unzip -y ​&&​ mkdir MHDDoS-old-but-powerful
 ​cd​ MHDDoS-old-but-powerful ​&&​ wget https://github.com/MHProDev/MHDDoS/releases/download/Beta/MHDDoS.v1.-.Beta.zip
 ​unzip MHDDoS.v1.-.Beta.zip ​&&​ ​cd​ .. ​&&​ ​cd​ MHDDoS
 pip install -r requirements.txt ​&&​ ​cd​ ​~
