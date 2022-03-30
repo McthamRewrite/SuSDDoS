e8374011d6c260ec109b8777da779262bb5ec8db
Fusedev <47820634+fusedevgithub@users.noreply.github.com>
2022-03-29 22:26:32 -0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 echo "
 ╱╱╱╱╱╱╱╱╱╱╱╭╮╱╭╮
 ╱╱╱╱╱╱╱╱╱╱╱┃┃╱┃┃
@@ -5,13 +6,17 @@ echo "
 ┃━━┫┃┃┃━━┫╭╮┃╭╮┃╭╮┃━━┫
 ┣━━┃╰╯┣━━┃╰╯┃╰╯┃╰╯┣━━┃
 ╰━━┻━━┻━━┻━━┻━━┻━━┻━━╯"
-sleep 1
 echo We are not responsible for any damage caused by this script.
-​mkdir ATTACK-PACK ​&&​ ​cd​ ATTACK-PACK
-​git clone https://github.com/Leeon123/TCP-UDP-Flood
-​git clone https://github.com/MHProDev/MHDDoS
-pkg update -y && pkg install python3-pip -y && pkg install python3 -y && ​pkg install wget -y && pkg install unzip -y ​&&​ mkdir MHDDoS-old-but-powerful
-​cd​ MHDDoS-old-but-powerful ​&&​ wget https://github.com/MHProDev/MHDDoS/releases/download/Beta/MHDDoS.v1.-.Beta.zip
-​unzip MHDDoS.v1.-.Beta.zip ​&&​ ​cd​ .. ​&&​ ​cd​ MHDDoS
-pip install -r requirements.txt ​&&​ ​cd​ ​~
-echo​ ​"​susddos is installing done! now cd ATTACK-PACK!​"
+sleep 1
+pkg update -y
+pkg install python3-pip -y 
+pkg install python3 -y 
+mkdir ATTACK-PACK && cd ATTACK-PACK 
+git clone https://github.com/Leeon123/TCP-UDP-Flood 
+git clone https://github.com/MHProDev/MHDDoS 
+pkg install wget -y 
+pkg install unzip -y && mkdir MHDDoS-old-but-powerful 
+cd MHDDoS-old-but-powerful && wget https://github.com/MHProDev/MHDDoS/releases/download/Beta/MHDDoS.v1.-.Beta.zip 
+unzip MHDDoS.v1.-.Beta.zip && cd .. && cd MHDDoS 
+pip install -r requirements.txt && cd ~
+echo "susddos is installing done! now cd ATTACK-PACK!"
